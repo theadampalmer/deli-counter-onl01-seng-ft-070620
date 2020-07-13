b2f31324ecac073a954fa33e452e9dc4d74fd5de
Adam Palmer <theadampalmer@gmail.com>
2020-07-13 23:56:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,31 @@
+katz_deli = []
 
+def line(katz_deli)
+  if katz_deli.empty?
+    puts "The line is currently empty."
+  elsif
+    positions = []
+    counter = 1
+    katz_deli.each {|name|
+      positions.push ("#{counter}. #{name}")
+      counter += 1
+    }
+    puts "The line is currently: #{positions.join(" ")}" 
+  end
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli.push ("#{name}")
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+
+
+
+def now_serving(katz_deli)
+  if katz_deli.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli[0]}."
+  katz_deli.shift
+  end
+end
