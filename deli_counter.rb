4aeccd5108fc232b68de9861c0c514b434411241
Adam Palmer <theadampalmer@gmail.com>
2020-07-13 23:51:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,1 @@
-require 'pry'
-
-katz_deli = []
-other_deli = ["Logan", "Avi", "Spencer"]
-another_deli = ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"]
-def line
 
